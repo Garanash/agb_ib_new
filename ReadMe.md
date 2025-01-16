505fd143ec76ce672e6e115030374b0baacdfe37
a.dolgov <dolgov_am@mail.ru>
2025-01-16 15:33:35 +0300

    create ReadMe.md

---
+++ ReadMe.md
@@ -0,0 +1,18 @@
+Для скачивания репозитория выполните команды
+
+```bash
+
+git clone https://github.com/Garanash/agb_ib_new.git
+cd agb_ib_new
+```
+
+Для запуска приложения откройте командную строку и выполните команды
+
+```bash
+
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+cd src
+python main.py
+```
---
+++ src/main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI, Request, Form, Path
+from fastapi.templating import Jinja2Templates
+from search.metizes.views import router as router_metiz
+from core import db
+
+templates = Jinja2Templates(directory="templates")  # регистрируем папку как папку с шаблонами джинджа
+
+app = FastAPI()
+app.include_router(router_metiz)
+
+
+@app.get('/')
+def main(request: Request):
+    return templates.TemplateResponse('start.html', {'request': request})
+
+
+if __name__ == '__main__':
+    db.create_table_metizes()
+    uvicorn.run('main:app', port=6789, reload=True)
